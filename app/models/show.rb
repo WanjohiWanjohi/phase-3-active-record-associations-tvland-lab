6fd9e6d88dce95353fcebe62cec316e303dbbc1e
WanjohiWanjohi <wamuyuwanjohi97@gmail.com>
2022-09-03 12:30:25 +0300

    commit tree 8a0fb59eedcd4d3bd09f2de951449a830f935757

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
+  has_many :characters, through: :actors
+  belongs_to :network
+  def characters
+    self.characters
+  end
+  def actors_list
+    self.actors
+  end
+
   
 end
